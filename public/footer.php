07a39a41087292d7e3a19b69e3c22d5bf24867bd
Matthew Sharman <zimmerson9@gmail.com>
2017-01-25 20:08:54 +0000

    Initial commit

---
+++ public/footer.php
@@ -0,0 +1,5 @@
+</div><!-- content -->
+</div><!-- wrapper -->
+<div id="footer">Created by Matthew Sharman&copy;</div>
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+require 'database.php';
+
+SELECT
+    categories.cat_id,
+    categories.cat_name,
+    categories.cat_description,
+FROM
+    categories
+?>
