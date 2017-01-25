402ab2aeca8e9c3ecee692e9e3b95e8f9916a1b5
Christopher Sharman <christopher.p.sharman@gmail.com>
2017-01-25 22:27:26 +0000

    Configuration updated so database credentials are loaded via environment files. Composer was added including the vlucas/phpdotenv library.

--- public/Database.php
+++ public/Database.php
@@ -1,74 +1,87 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Matthew
- * Date: 19/12/2016
- * Time: 20:58
- */
-
-
-
 
 class Database
 {
+
+    /** @var self An instance if this class. */
     static $instance;
+
+    /** @var PDO The database connection. */
     private $pdo;
 
-    private function __construct()
+    /**
+     * Database constructor.
+     *
+     * @param string $dbHost Database host.
+     * @param string $dbName Database name.
+     * @param string $dbUser Database user.
+     * @param string $dbPass Password for the database user.
+     */
+    private function __construct($dbHost, $dbName, $dbUser, $dbPass)
     {
-        $this->pdo = new \PDO('mysql:host=localhost;dbname=forum', 'homestead', 'secret');
+        $this->pdo = new \PDO("mysql:host=$dbUser;dbname=$dbName", $dbUser, $dbPass);
     }
 
     public static function getInstance()
     {
         if (is_null(self::$instance)) {
-            self::$instance = new self();
+
+            // Load the credentials for use.
+            $dbHost = getenv('DB_HOST');
+            $dbName = getenv('DB_NAME');
+            $dbUser = getenv('DB_USER');
+            $dbPass = getenv('DB_PASS');
+
+            self::$instance = new self($dbHost, $dbName, $dbUser, $dbPass);
         }
 
         return self::$instance;
-    }
-
-    public function getforum()
-    {
-        $stmt = $this->pdo->prepare('SELECT * FROM todos WHERE deleted_at IS NULL');
-        $stmt->execute();
-        $todos = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-
-        return $todos;
-    }
-
-    public function addTodo($summary, $isImportant)
-    {
-        $stmt = $this->pdo->prepare("
-            INSERT INTO forum (`summary`, `is_important`)
-            VALUES (:summary, :isImportant)
-        ");
-
-        $stmt->execute([
-            'summary' => $summary,
-            'isImportant' => $isImportant ? 1 : 0
-        ]);
-        
-    }
-
-    public function updateTodo($id, $fieldsAndValues)
-    {
-        $sets = [];
-        foreach ($fieldsAndValues as $field => $value) {
-            $sets[] = "$field = '$value'";
-        }
-        $sets = implode(', ', $sets);
-        
-        $stmt = $this->pdo->prepare("
-            UPDATE todos
-            SET $sets
-            WHERE id = $id AND deleted_at IS NULL
-        ");
-        $stmt->execute();
     }
 
     public function getPDO()
     {
         return $this->pdo;
     }
+
+    // TODO: Rewrite the below for this project.
+
+//    public function getforum()
+//    {
+//        $stmt = $this->pdo->prepare('SELECT * FROM todos WHERE deleted_at IS NULL');
+//        $stmt->execute();
+//        $todos = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+//
+//        return $todos;
+//    }
+//
+//    public function addTodo($summary, $isImportant)
+//    {
+//        $stmt = $this->pdo->prepare("
+//            INSERT INTO forum (`summary`, `is_important`)
+//            VALUES (:summary, :isImportant)
+//        ");
+//
+//        $stmt->execute([
+//            'summary' => $summary,
+//            'isImportant' => $isImportant ? 1 : 0
+//        ]);
+//
+//    }
+//
+//    public function updateTodo($id, $fieldsAndValues)
+//    {
+//        $sets = [];
+//        foreach ($fieldsAndValues as $field => $value) {
+//            $sets[] = "$field = '$value'";
+//        }
+//        $sets = implode(', ', $sets);
+//
+//        $stmt = $this->pdo->prepare("
+//            UPDATE todos
+//            SET $sets
+//            WHERE id = $id AND deleted_at IS NULL
+//        ");
+//        $stmt->execute();
+//    }
+
 }
--- public/index.php
+++ public/index.php
@@ -1,10 +1,3 @@
 <?php
-require 'database.php';
 
-SELECT
-    categories.cat_id,
-    categories.cat_name,
-    categories.cat_description,
-FROM
-    categories
-?>
+require 'Database.php';
