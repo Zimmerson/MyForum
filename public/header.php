81b92b68b38198be6a360919bd9bb5cc0e543546
Matthew Sharman <zimmerson9@gmail.com>
2017-01-26 23:09:54 +0000

    Updated header.php. Fixed broken links.

--- public/header.php
+++ public/header.php
@@ -12,16 +12,16 @@
 <h1>My forum</h1>
 <div id="wrapper">
     <div id="menu">
-        <a class="item" href="/forum/index.php">Home</a> -
-        <a class="item" href="/forum/create_topic.php">Create a topic</a> -
-        <a class="item" href="/forum/create_cat.php">Create a category</a>
+        <a class="item" href="index.php">Home</a> -
+        <a class="item" href="create_topic.php">Create a topic</a> -
+        <a class="item" href="create_cat.php">Create a category</a>
 
         <div id="userbar">
             <?php
             if (isset($_SESSION['signed_in']) && $_SESSION['signed_in']) {
                 echo 'Hello' . $_SESSION['user_name'] . '. Not you? <a href="signout.php">Sign out</a>';
             } else {
-                echo '<a href="signin.php">Sign in</a> or <a href="sign up">create an account</a>.';
+                echo '<a href="signin.php">Sign in</a> or <a href="signup.php">create an account</a>.';
             }
             ?>
         </div>
--- public/signup.php
+++ public/signup.php
@@ -13,7 +13,7 @@ if($_SERVER['REQUEST_METHOD'] != 'POST')
         Password: <input type="password" name="user_pass">
         Password again: <input type="password" name="user_pass_check">
         E-mail: <input type="email" name="user_email">
-        <input type="submit" value="Add category" />
+        <input type="submit" value="create account" />
      </form>';
 }
 else
